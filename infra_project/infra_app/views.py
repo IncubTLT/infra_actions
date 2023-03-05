4c9e8d5e04477c7c7b5999dbb86548dded70f6e6
Евгений Волков <evgeniy.volkov@me.com>
2023-03-05 13:50:39 +0400

    Try a server

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Кажется, теперь у меня получилось!')
+    return HttpResponse('Теперь у меня получилось!')
 
 
 def second_page(request):
